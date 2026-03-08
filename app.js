7c98107e5129413f6460690dde50f6d9c4ec00c8
Vitor de Paula Santos <vitorpaulasantos@gmail.com>
2026-03-08 16:05:41 -0300

    organizando as rotas por arquivos

--- app.js
+++ app.js
@@ -2,41 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-import Order;
-
 app.use(express.json());
 
-// 1. POST: Um novo pedido.
-app.post('/order', (req, res) => {
-    try {
-    	const novaOrder = await Order.create(req.body);
-    	res.status(201).json(novaOrder);
-    } catch (error) {
-    	res.status(400).json({ mensagem: "Erro ao criar uma order", erro: error.message });
-    }
-});
-
-// 2. GET: Listar todos os pedidos.
-app.get('/order/list', (req, res) => {
-    
-});
-
-// 3. GET: Buscar um pedido específico pelo ID.
-app.get('/order/:id', (req, res) => {
-    
-});
-
-
-// 4. PUT: Atualizar um pedido existente.
-app.put('/order/:id', (req, res) => {
-    
-});
-
-// 5. DELETE: Remover um pedido.
-app.delete('/order/:id', (req, res) => {
-    
-});
-
+const postOrder = require('./Routes/createOrder');
+const getListOrder = require('./Routes/listOrder');
+const getOneOrder = require('./Routes/getOneOrder');
+const updateOrder = require('./Routes/updateOrder');
+const deleteOrder = require('./Routes/deleteOrder');
 
 // Iniciar o servidor
 app.listen(PORT, () => {
